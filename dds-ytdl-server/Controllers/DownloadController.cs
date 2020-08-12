5df30e9ef6214c1ea968a554a245bb89549abad3
davdesch <david.de.schrijver@gmail.com>
2020-08-12 23:58:17 +0200

    Fix bug de black pink les tchoins

--- dds-ytdl-server/Controllers/DownloadController.cs
+++ dds-ytdl-server/Controllers/DownloadController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using YoutubeExplode;
 using YoutubeExplode.Converter;
@@ -17,13 +19,13 @@ namespace dds_ytdl_server.Controllers
             var converter = new YoutubeConverter(client);
             var streamManifest = await client.Videos.Streams.GetManifestAsync(id);
             var title = client.Videos.GetAsync(id).Result.Title;
-            string path = title + ".mp3";
+            string path = $"{id}.mp3";
             var streamInfo = streamManifest.GetAudioOnly().WithHighestBitrate();
             var mediaStreamInfos = new IStreamInfo[] { streamInfo };
             await converter.DownloadAndProcessMediaStreamsAsync(mediaStreamInfos, path, "mp3");
             byte[] buff = System.IO.File.ReadAllBytes(path);
             System.IO.File.Delete(path);
-            return File(buff, "application/force-download", path);
+            return File(buff, "application/force-download", $"{title}.mp3");
         }
     }
 }
